aef9f9659e1e80e3992770a78ee170c2f80e9aac
Taizo 'Tsa6' Simpson <taizo@tsa6.tk>
2017-08-05 15:01:25 -0400

    Added additional info to the website

--- avoidsafari/urls.py
+++ avoidsafari/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
+    url(r'^info$', TemplateView.as_view(template_name="info.html")),
     url(r'^$', views.MainView.as_view()),
 ]
